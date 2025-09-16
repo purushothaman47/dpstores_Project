be379bfe8d1ba8fffd3939c344fa85ee556735f2
purushothaman47 <144509020+purushothaman47@users.noreply.github.com>
2025-09-17 01:16:12 +0530

    Create DpStoreApplication.java

---
+++ DpStoreApplication.java
@@ -0,0 +1,13 @@
+package com.dpcart.dp_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpStoreApplication.class, args);
+	}
+
+}
